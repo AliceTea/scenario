eef37b1bef29c6b606862957103f6bb418546209
JasonTang <jxtang@shesi.org>
2017-06-22 00:37:10 +0800

    update

--- cbr003.py
+++ cbr003.py
@@ -8,6 +8,8 @@ from mininet.net import Mininet
 from mininet.util import irange,dumpNodeConnections
 from mininet.log import setLogLevel
 from mininet.node import RemoteController
+from xml.dom.minidom import parse
+import xml.dom.minidom
 
 class SimpleTopo(Topo):
 
@@ -33,49 +35,23 @@ class ComplexTopo(SimpleTopo):
             ):
         if filename == '':
             super(ComplexTopo,self).__init__()
-        else:
 
 
     def parseconf(self,filename):
-        conf = open(filename,'r')
-        lines = conf.readlines()
-        topoflag = 0
-        for line in lines:
-            if topoflag == 1:
-                #parse configuration
-                nodes = re.findall(r'h\d+->s\d+',line)
-                hosts = re.findall(r'h\d+',nodes[0])
-                switches = re.findall(r's\d+',nodes[0])
-                if nodes.__len__() == 1 and switches.__len__() == 1:
-                    host = self.addHost(str(hosts[0]))
-                    sw = self.addSwitch(str(switches[0]))
-                    self.addLink(host,sw)
-                    
+        DOMTree = parse(filename)
+        collection = DOMTree.documentElement
+        switches = collection.getElementsByTagName("switch")
+        hosts = collection.getElementsByTagName("host")
+        controller = collection.getElementsByTagName("controller")
+        links = collection.getElementsByTagName("link")
 
-            if line == 'topo:':
-                topoflag = 1
-                
+        print switches[0].getAttribute("title")
+        print hosts
+        print controller
+
 
 
 if __name__ == '__main__':
 
-    f = open(str(sys.argv[1]),'r')
-    
-    setLogLevel('info')
-    topo = SimpleTopo()
-    net = Mininet(topo,controller=partial(RemoteController,ip='127.0.0.1',port=6653))
-    net.start()
-    dumpNodeConnections(net.hosts)
-    time.sleep(10)
-    for sw in net.switches:
-        os.system('sudo ovs-vsctl set bridge s1 protocols=OpenFlow13')
-        
-    lines = f.readlines()
-    for line in lines:
-        m = re.findall(r'\w+',line)
-        print str(m[0])
-        print int(m[1])
-        net.iperf(net.hosts,'UDP',str(m[0]),None,int(m[1]),8080)
-         
-    time.sleep(10)
-    net.stop()
+    tmp = ComplexTopo()
+    tmp.parseconf('/home/jason/workspace/scenario/xml-demo.xml')
