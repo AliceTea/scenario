cf6b0fd35251a87fb1c6ad4307e98f1cc6e4104c
jasontang1992 <jasontang666@163.com>
2017-12-26 15:20:16 +0800

    update

--- algs.py
+++ algs.py
@@ -5,21 +5,110 @@ Created on Thu Dec 21 17:30:09 2017
 @author: jason
 """
 
+import math
+
+ws_max=3
+ws=1
+
 def fuzz_arima(timelist,statslist):
+    
     pass
 
 def payless(duration,timelist,statslist):
     if len(statslist) < 1:
         return duration
-    if statslist[len(statslist)-1] > 5000000:
+    if statslist[len(statslist)-1] > 4900000:
         return max(duration/3,1)
-    elif statslist[len(statslist)-1] < 3000000:
+    elif statslist[len(statslist)-1] < 950000:
         return min(duration*2,100)
     else:
         return duration
 
-def swd(duration,timelist,statslist):
-    pass
+def swt(duration,timelist,statslist,duration_max):
+#   duration_max=100
+    duration_min=1
+    ret = 0
+    win=[]
+    global ws
+    if len(statslist)<1:
+#        print 'swt init'
+        ws=3
+    if(len(statslist)>1):
+        var=statslist[len(statslist)-1]
+        i=len(statslist)-2
+        while len(win)<ws and i>=0:
+            win.append(statslist[i])
+            i=i-1
 
-def adarate(duration,timelist,statslist):
-    pass
+        mean = 0
+        for dat in win:
+            mean = mean + dat
+        mean=mean/len(win)
+        stdev = 0
+ 
+        for dat in win:
+            stdev = stdev + (dat-mean)*(dat-mean)
+        stdev = math.sqrt(stdev/(len(win)))
+
+    else:
+        mean = 0
+        stdev = 0
+        var=0
+    
+    if var > mean + 2*stdev:
+        ret = max(duration/2,duration_min)
+        ws=min(3,ws/2)
+    else:
+        ret = min(duration*2,duration_max)
+        ws=ws+1
+    if len(win)>ws:
+        win.remove(win[0])
+#    print 'stdev:'+str(stdev)+' mean:'+str(mean)+' var:'+str(var)+' ws:'+str(ws)
+    return ret
+
+def adarate(duration,timelist,statslist,duration_max):
+#    duration_max=100
+    duration_min=1
+    ret = duration
+    win=[]
+    global ws_max
+    global ws
+    if len(statslist)<1:
+#        print 'adarate init'
+        ws_max=3
+        ws=1
+    if(len(statslist)>1):
+        i=len(statslist)-1
+        while len(win)<ws and i>=0:
+            if i > 0:
+                win.append(statslist[i]/(timelist[i]-timelist[i-1]))
+            else:
+                win.append(statslist[i]/(timelist[i]))
+            i=i-1
+
+        mean = 0
+        for dat in win:
+            mean = mean + dat
+        mean=mean/len(win)
+        stdev = 0
+ 
+        for dat in win:
+            stdev = stdev + (dat-mean)*(dat-mean)
+        stdev = math.sqrt(stdev/(len(win)))
+
+    else:
+        mean = 0
+        stdev = 0
+   
+    if stdev > 0.2*mean:
+        ret = max(duration/2,duration_min)
+        ws_max=min(3,ws_max/2)
+#        ws=1
+    else:
+        ret = min(duration*2,duration_max)
+        ws_max=ws_max+1
+        ws=ws+1
+    if len(win)>ws_max and ws>1:
+        ws=ws-1
+#    print 'stdev:'+str(stdev)+' mean:'+str(mean)+ ' ws:'+str(ws) + ' ws_max:'+str(ws_max)
+    return ret
--- simulator_base.py
+++ simulator_base.py
@@ -9,16 +9,50 @@ import sys
 import re
 import algs
 import matplotlib.pyplot as plt
+import math
+import pandas as pd
+import numpy as np
+
+def interpolate(method='default',timelist=[],ratelist=[]):
+    i=0
+    while i < timelist[len(timelist)-1]:
+        if timelist[i] > i+1:
+            timelist.insert(i,i+1)
+            ratelist.insert(i,np.nan)
+        else:
+            i=i+1
+    ratelist=pd.Series(data=ratelist)
+    ratelist.interpolate('cubic')
+    print ratelist
+    plt.plot(ratelist)
 
 
-def measure(duration,timelist,statslist,method):
+def predict(timelist,statslist,method,*args):
+    ret = 0
+    if method=='fuzz_arima':
+        pass
+    elif method=='fuzz_RNN':
+        pass
+    elif method=='RNN':
+        pass
+    elif method=='arima':
+        pass
+    return ret
+
+def measure(duration,timelist,statslist,method,*args):
     ret=1
     if method=='default':
         ret=1
-    elif method=='SWD':
-        pass
+    elif method=='SWT':
+        if len(args)!=1:
+            ret = algs.swt(duration,timelist,statslist,10)
+        else:
+            ret = algs.swt(duration,timelist,statslist,args[0])
     elif method=='adarate':
-        pass
+        if len(args)!=1:
+            ret = algs.adarate(duration,timelist,statslist,10)
+        else:
+            ret = algs.adarate(duration,timelist,statslist,args[0])
     elif method=='fuzz_arima':
         ret = algs.fuzz_arima(timelist,statslist)
     elif method=='payless':
@@ -33,47 +67,145 @@ The measured data need interpolation.
 '''
 
 def errorate(timelist=[],ratelist=[],realratelist=[]):
-    error = 0
+    i=0 
+    while len(realratelist) > len(timelist):
+        if timelist[i] > i+1:
+            timelist.insert(i,i+1)
+            ratelist.insert(i,ratelist[i-1])
+        else:
+            i=i+1
     i=0
-    tmp=0
-    while i<len(timelist):
-        pass
-    pass
+    error=0
+    while i<len(realratelist):
+        if realratelist[i]==0:
+            error=error+0
+        else:
+            error=error+((realratelist[i]-ratelist[i])/float(realratelist[i]))*((realratelist[i]-ratelist[i])/float(realratelist[i]))
+        i=i+1
+    return math.sqrt(error/len(realratelist))
+#    pass
+#    print timelist
+#    plt.plot(timelist,ratelist)
     
 def overhead(ratelist):
+    return len(ratelist)
     pass
 
+def sort(lA,lB):
+    i=0
+    while i<len(lA):
+        i=i+1
+        j=0
+        flag=0
+        while j<len(lA)-1:
+            if(lA[j]>lA[j+1]):
+                tmp = lA[j+1]
+                lA[j+1] = lA[j]
+                lA[j] = tmp
+                tmp = lB[j+1]
+                lB[j+1] = lB[j]
+                lB[j] = tmp
+                flag=1
+            j=j+1
+        if flag==0:
+            return
 
-
-file = open(r'/media/jason/Seagate Backup Plus Drive/实验数据/2017年12月11日UTC.datx','r+')
-ts=[]
-cnt=0
-duration=1
-statslist=[]
-timelist=[]
-ratelist=[]
-method='payless'
-line=file.readline()
-loop=0
-count=0
-ts=0
-while line != "":
-#    cnt=cnt+1
-#    print cnt
-    duration=measure(duration,timelist,statslist,method)
-    assert duration>0,'duration is illedga'
-    loop=duration
-    while loop>0:
-        loop=loop-1
-        line=file.readline()
-        data=re.findall('\d+',line)
-        if(data==[]):
-            break
-        count=count+int(data[1])
-    ts=ts+duration
-    ratelist.append(count/duration)
-    statslist.append(count)
-    timelist.append(ts)
+def method_alltest(methods):
+    lA=[] #list of overhead
+    lB=[] #list of error rate
+    ret=tuple()
+    if methods == 'payless':
+        pass
+    elif methods == 'SWT':
+        i=1
+        while i<100:
+            i=i*2
+            ret=method_test(methods,i)
+            lA.append(ret[0])
+            lB.append(ret[1])
+    elif methods == 'adarate':
+        i=1
+        while i<100:
+            i=i*2
+            ret=method_test(methods,i)
+            lA.append(ret[0])
+            lB.append(ret[1])
+    sort(lA,lB)
+    plt.plot(lA,lB,'^-')
+    print lA
+    print lB
+    return lA,lB
+            
+def method_test(methods,*args):
+    file = open(r'/media/jason/Seagate Backup Plus Drive/实验数据/2017年12月11日UTC.datx','r+')
+    ts=[]
+    duration=1
+    statslist=[]
+    timelist=[]
+    ratelist=[]
+    method=methods
+    line=file.readline()
+    loop=0
     count=0
+    ts=0
+    while line != "":
+    #    cnt=cnt+1
+    #    print cnt
+        if len(args)!=1:
+            duration=measure(duration,timelist,statslist,method)
+        else:
+            duration=measure(duration,timelist,statslist,method,args[0])
+        assert duration>0,'duration is illedga'
+        loop=duration
+        while loop>0:
+            loop=loop-1
+            line=file.readline()
+            data=re.findall('\d+',line)
+            if(data==[]):
+                break
+            count=count+int(data[1])
+        ts=ts+duration
+        ratelist.append(count/duration)
+        statslist.append(count)
+        timelist.append(ts)
+        count=0
+            
+    file = open(r'/media/jason/Seagate Backup Plus Drive/实验数据/2017年12月11日UTC.datx','r+')
+    ts=[]
+    duration=1
+    statslist=[]
+    realratelist=[]
+    method='default'
+    line=file.readline()
+    loop=0
+    count=0
+    ts=0
+    while line != "":
+    #    cnt=cnt+1
+    #    print cnt
+        duration=measure(duration,timelist,statslist,method)
+        assert duration>0,'duration is illedga'
+        loop=duration
+        while loop>0:
+            loop=loop-1
+            line=file.readline()
+            data=re.findall('\d+',line)
+            if(data==[]):
+                break
+            count=count+int(data[1])
+        ts=ts+duration
+        realratelist.append(count/duration)
+        statslist.append(count)
+        count=0
+    
+#    print 'overhead:'+str(overhead(ratelist))
+#    print 'error:'+str(errorate(timelist,ratelist,realratelist))
+    return overhead(ratelist),errorate(timelist,ratelist,realratelist)
 
-plt.plot(timelist,ratelist)
+#method_test('payless')
+#method_test('SWT')
+#method_test('payless')
+
+method_alltest('adarate')
+method_alltest('SWT')
+plt.show()
